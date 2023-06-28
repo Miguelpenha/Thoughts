0946ae93a496469eda3f74f98898fb96923d1d1e
Miguel Penha <miguel.penha1@gmail.com>
2023-06-28 00:08:12 -0300

    Update index.tsx

--- components/modalizes/modalizeMenuIcons/index.tsx
+++ components/modalizes/modalizeMenuIcons/index.tsx
@@ -24,7 +24,7 @@ function modalizeMenuIcons(setHeight: Dispatch<SetStateAction<number>>, modalize
             },
             renderItem: ({ index, item }: { index: number, item: string }) => (
                 item.toUpperCase().includes(filter.toUpperCase()) && (
-                    <ButtonIcon index={index} onPress={() => {
+                    <ButtonIcon index={index/2} onPress={() => {
                         setIcon(item)
 
                         modalize.current.close()
