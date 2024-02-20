91302df45fc641f7ba2aa3734bf2abb08a8d6543
Miguel Penha <miguel.penha1@gmail.com>
2024-02-20 01:51:22 -0300

    Update useAnimationInputSearch.ts

--- components/ListThoughts/useAnimationInputSearch.ts
+++ components/ListThoughts/useAnimationInputSearch.ts
@@ -1,7 +1,7 @@
+import { AnimatedProps, useSharedValue, useAnimatedStyle, withTiming, FadeInDown } from 'react-native-reanimated'
 import { TextInputProps } from 'react-native'
-import { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated'
 
-function useAnimationInputSearch(): TextInputProps {
+function useAnimationInputSearch(): AnimatedProps<TextInputProps> {
     const border = useSharedValue(18)
 
     const animation = useAnimatedStyle(() => ({
@@ -9,6 +9,7 @@ function useAnimationInputSearch(): TextInputProps {
     }))
 
     return {
+        entering: FadeInDown.delay(400).duration(400),
         style: [animation],
         onFocus() {
             border.value = withTiming(8)
